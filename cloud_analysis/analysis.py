35abc0b971a824ab4f7fee6d7596fcc1ec98d12d
Daniel7609 <danielmeiborg@icloud.com>
2022-01-29 15:50:24 +0100

    added raise Exceptions to analysis.py

--- cloud_analysis/analysis.py
+++ cloud_analysis/analysis.py
@@ -7,13 +7,29 @@ from cloud_detection.cloud_filter import CloudFilter
 # see http://www.cyto.purdue.edu/cdroms/micro2/content/education/wirth06.pdf
 # and http://www.cyto.purdue.edu/cdroms/micro2/content/education/wirth10.pdf
 
-
 BORDER_DISTANCE = 972
+
+
+def check_type(var, val, msg: str):
+    if var != val:
+        raise TypeError(msg)
+
+
+def check_value(val, msg: str):
+    if not val:
+        raise ValueError(msg)
 
 
 class Analysis:
 
     def __init__(self, orig: np.ndarray, num_clouds: int, border_threshold: float = 0.1, border_width: int = 5):
+        check_type(orig, np.ndarray, 'The image should be a numpy array')
+        check_type(len(orig.shape), 3, 'The image has to be an array of the shape (height, width, channels)')
+        check_type(orig.shape[2], 3, 'The image has to be a RGB image')
+        check_type(num_clouds, int, 'The number of clouds has to be an integer')
+        check_type(border_threshold, float, 'The threshold for the border proportion of the clouds has to be a number')
+        check_type(border_width, int, 'The width of the border has to be an integer (in pixel)')
+
         self.orig = orig
         self.height, self.width, self.channels = self.orig.shape
         self.center = np.array([self.height / 2, self.width / 2])
@@ -22,12 +38,11 @@ class Analysis:
         self.contours = self._get_contours(num_clouds, border_threshold, border_width)
         self.clouds = self._get_clouds()
 
-    def __str__(self) -> str:
-        return f'dimensions: {self.orig.shape}\nnumber of clouds: {len(self.clouds)}'
-
     def _get_mask(self) -> np.ndarray:
         cloud_filter = CloudFilter()
         mask, _ = cloud_filter.evaluate_image(self.orig)
+        check_value(np.any(mask), 'the image has no clouds')
+
         return cv.resize(mask, (self.width, self.height))
 
     def _get_contours(self, num_clouds: int, border_threshold: float, border_width: int) -> np.ndarray:
@@ -35,6 +50,7 @@ class Analysis:
         # get a tuple of arrays of all contours in the image
         all_contours, _ = cv.findContours(cv.medianBlur(self.mask, 3), cv.RETR_CCOMP, cv.CHAIN_APPROX_NONE)
         all_contours = [np.squeeze(contour) for contour in all_contours]
+        check_value(len(all_contours) < num_clouds, 'The image has not enough clouds to return')
 
         # get the distance from the center for each point of the contours
         norm_contours = [np.linalg.norm(contour - self.center, axis=1) for contour in all_contours]
@@ -52,6 +68,8 @@ class Analysis:
         # get all contours whose border ratio does not exceed a specific threshold
         non_border_contours = [contour for i, contour in enumerate(all_contours)
                                if border_ratios[i] <= border_threshold]
+        check_value(len(non_border_contours) < num_clouds, 'The image has not enough clouds'
+                                                           'which are inside the visible area to return')
 
         # take the largest contours
         areas = np.array([cv.contourArea(contour) for contour in non_border_contours])
@@ -80,9 +98,6 @@ class Analysis:
             self.shape = self.Shape(self.contour)
             self.texture = self.Texture(self.img, self.mask)
 
-        def __str__(self) -> str:
-            return f'dimensions: {self.img.shape}\n'
-
         class Shape:
 
             def __init__(self, contour: tuple):
@@ -92,20 +107,6 @@ class Analysis:
                 self.hull = cv.convexHull(self.contour)
                 self.hull_perimeter = cv.arcLength(self.hull, True)
                 self.hull_area = cv.contourArea(self.hull)
-
-            def __str__(self) -> str:
-                out = [f'Shape Analysis:',
-                       f'   contour perimeter: {self.contour_perimeter}',
-                       f'   hull perimeter: {self.hull_perimeter}',
-                       f'   contour area: {self.contour_area}',
-                       f'   hull area: {self.hull_area}',
-                       f'   roundness: {self.roundness()}',
-                       f'   rectangularity: {self.rectangularity()}',
-                       f'   convexity: {self.convexity()}',
-                       f'   compactness: {self.compactness()}',
-                       f'   solidity: {self.solidity()}',
-                       f'   elongation: {self.elongation()}', ]
-                return '\n'.join(out)
 
             def roundness(self) -> float:
                 return (4 * np.pi * self.contour_area) / (self.hull_perimeter ** 2)
@@ -131,21 +132,15 @@ class Analysis:
             def __init__(self, img: np.ndarray, mask: np.ndarray):
                 self.img = img
                 self.mask = mask
-                self.grey = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
-
-            def __str__(self) -> str:
-                out = ['Texture Analysis:',
-                       f'   mean: {self.mean()}',
-                       f'   standard deviation: {self.std()}',
-                       f'   transparency: {self.transparency()}', ]
-                return '\n'.join(out)
+                self.grey = cv.cvtColor(self.img, cv.COLOR_RGB2GRAY)
 
             def distribution(self) -> list:
                 data = []
                 for n in range(3):
                     channel = np.array(self.img[:, :, n].ravel())
-                    data.append(channel[channel.nonzero()])
-                return data
+                    non_zero = channel[channel.nonzero()]
+                    data.append(non_zero)
+                return np.array(data)
 
             def mean(self) -> list:
                 return cv.mean(self.img, mask=self.mask)
@@ -158,7 +153,11 @@ class Analysis:
             def transparency(self) -> float:
                 sat = cv.cvtColor(self.img, cv.COLOR_RGB2HSV)[:, :, 0]
                 inverse = np.where(sat == 0, sat, 255 - sat)
-                return np.sum(inverse) / np.count_nonzero(inverse)
+                if np.count_nonzero(inverse) != 0:
+                    out = np.sum(inverse) / np.count_nonzero(inverse)
+                else:
+                    out = 0
+                return out
 
         def altitude(self):
             # https://en.wikipedia.org/wiki/Cloud_base#Measurement
@@ -169,10 +168,15 @@ class Analysis:
             pass
 
         def mean_diff_edges(self, num_samples: int, in_steps: int, out_steps: int,
-                            regr_distance: int = 3, regr_length: float = 1) -> float:
+                            regr_dist: int = 3, regr_len: float = 1) -> float:
+            check_type(num_samples, int, 'num_samples should be an integer')
+            check_type(in_steps, int, 'in_steps should be an integer')
+            check_type(out_steps, int, 'out_steps should be an integer')
+            check_type(regr_dist, int, 'regr_dist should be an integer')
+            check_type(regr_len, float, 'regr_length')
 
-            regr_vectors = np.roll(self.contour, regr_distance, axis=0) - self.contour
-            regr_vectors = regr_vectors * regr_length / np.tile(np.linalg.norm(regr_vectors, axis=1), (2, 1)).T
+            regr_vectors = np.roll(self.contour, regr_dist, axis=0) - self.contour
+            regr_vectors = regr_vectors * regr_len / np.tile(np.linalg.norm(regr_vectors, axis=1), (2, 1)).T
 
             perp_vectors = np.roll(regr_vectors, 1, axis=1)
             perp_vectors[:, 0] *= -1
