5290b77ccbe952ce61dc0b195141db6f7e56f561
Daniel7609 <danielmeiborg@icloud.com>
2022-02-02 18:58:59 +0100

    updated test_analysis.py

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,11 +1,12 @@
 import cv2 as cv
 import cloud_analysis.analysis
 
-image = cv.imread('../sample_data/Data/zz_astropi_1_photo_364.jpg')
-
 
 def test_is_not_none():
-    analysis = cloud_analysis.analysis.Analysis(image, 5, 0.1)
+    img = cv.imread('../sample_data/Data/zz_astropi_1_photo_364.jpg')
+    assert img is not None
+
+    analysis = cloud_analysis.analysis.Analysis(img, 5, 0.1)
     assert analysis is not None
     assert analysis.clouds is not None
     assert analysis.orig is not None
@@ -15,7 +16,7 @@ def test_is_not_none():
     assert all([cloud.contour is not None for cloud in analysis.clouds])
     assert all(
         [cloud.contour_perimeter is not None for cloud in analysis.clouds])
-    assert all([cloud.contour_all is not None for cloud in analysis.clouds])
+    assert all([cloud.contour_area is not None for cloud in analysis.clouds])
     assert all([cloud.hull is not None for cloud in analysis.clouds])
     assert all([cloud.hull_perimeter is not None for cloud in analysis.clouds])
     assert all([cloud.hull_area is not None for cloud in analysis.clouds])
