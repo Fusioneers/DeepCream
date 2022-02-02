29e5fd9032405c500a556372ffb2a001a5b8bc4c
Daniel7609 <danielmeiborg@icloud.com>
2022-02-02 18:12:49 +0100

    added is not None tests to analysis_test.py

--- tests/analysis_test.py
+++ tests/analysis_test.py
@@ -5,15 +5,32 @@ from ..cloud_analysis.analysis import Analysis
 image = cv.imread('../sample_data/Data/zz_astropi_1_photo_364.jpg')
 
 
-def test_analysis_init():
+def test_is_not_none():
     analysis = Analysis(image, 5, 0.1)
     assert analysis is not None
+    assert analysis.clouds is not None
+    assert analysis.orig is not None
+    assert analysis.mask is not None
+    assert all([cloud is not None for cloud in analysis.clouds])
 
-# dtime = time.time()
-# for cloud in analysis.clouds:
-#     try:
-#         print(cloud.mean_diff_edges(3, 50, 500))
-#     except Exception:
-#         print(Exception)
-#
-# print(f'time: {time.time() - dtime}')
+    assert all([cloud.contour is not None for cloud in analysis.clouds])
+    assert all(
+        [cloud.contour_perimeter is not None for cloud in analysis.clouds])
+    assert all([cloud.contour_all is not None for cloud in analysis.clouds])
+    assert all([cloud.hull is not None for cloud in analysis.clouds])
+    assert all([cloud.hull_perimeter is not None for cloud in analysis.clouds])
+    assert all([cloud.hull_area is not None for cloud in analysis.clouds])
+
+    assert all([cloud.roundness() is not None for cloud in analysis.clouds])
+    assert all([cloud.convexity() is not None for cloud in analysis.clouds])
+    assert all([cloud.compactness() is not None for cloud in analysis.clouds])
+    assert all([cloud.solidity() is not None for cloud in analysis.clouds])
+    assert all(
+        [cloud.rectangularity() is not None for cloud in analysis.clouds])
+    assert all([cloud.elongation() is not None for cloud in analysis.clouds])
+    assert all([cloud.mean() is not None for cloud in analysis.clouds])
+    assert all([cloud.std() is not None for cloud in analysis.clouds])
+    assert all([cloud.transparency() is not None for cloud in analysis.clouds])
+    assert all([cloud.edges(50, 50) is not None for cloud in analysis.clouds])
+    assert all([cloud.mean_diff_edges(50, 50) is not None for cloud in
+                analysis.clouds])
