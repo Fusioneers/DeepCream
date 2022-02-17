e2f29c6720f88cc9c4b344a6f47f002c184377a4
Daniel Meiborg <79795628+DanielMeiborg@users.noreply.github.com>
2022-02-17 16:24:12 +0100

    minor changes

--- main.py
+++ main.py
@@ -10,26 +10,28 @@ start_time = time.time()
 logger = logging.getLogger('DeepCream.main')
 
 finished = False
-runtime = 0.005  # time the program is allowed to run (in hours)
+# TODO convert to seconds
+runtime = 0.005 * 60 * 60  # time the program is allowed to run (in hours)
 
-while int(time.time() - start_time) < (runtime * 60 * 60) and not finished:
+while int(time.time() - start_time) < runtime and not finished:
     # Makes sure the DeepCream module keeps running for the whole time
     try:
         deepcream = DeepCream.initialize(os.path.join(ABS_PATH, 'data'),
                                          tpu_support=False)
         logger.info('Initialised DeepCream')
-        allowed_execution_time = (runtime * 60 * 60) - (
+        allowed_execution_time = runtime - (
             int(time.time() - start_time))
-        logger.info('Calling DeepCream.run with ' + str(
-            allowed_execution_time) + 's of execution time')
+        logger.info(f'Calling DeepCream.run with {allowed_execution_time}s '
+                    f'of execution time')
         deepcream.run(allowed_execution_time)
         finished = True
-        logger.info('DeepCream execution time: ' + str(
-            int(time.time() - start_time)) + 's')
+        logger.info(
+            f'DeepCream execution time: {int(time.time() - start_time)}s')
     except Exception as err:
         logger.error(err)
+        # TODO Keyboard interrupt
 
 end_time = time.time()
 
-logger.info("Overall execution time: " + str(int(end_time - start_time)) + 's')
-print("Overall execution time: " + str(int(end_time - start_time)) + 's')
+logger.info(f'Overall execution time: {int(end_time - start_time)}s')
+print(f'Overall execution time: {int(end_time - start_time)}s')
